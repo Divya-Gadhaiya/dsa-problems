3708b59a668695912757bb4f9cdaad21720acd2d
Divya-Gadhaiya <66269157+Divya-Gadhaiya@users.noreply.github.com>
2020-06-24 23:40:51 +0530

    Create Count Complete Tree Nodes.js

---
+++ Count Complete Tree Nodes.js
@@ -0,0 +1,17 @@
+Given a complete binary tree, count the number of nodes.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function(root) {
+    return (!root)? 0 : 1+countNodes(root.left) + countNodes(root.right);
+};
